ef80e9e1055066ed7176141cf96e2f5a7a2fce51
hooni0212 <kingth0212@gmail.com>
2026-03-31 08:30:18 +0900

    chore(release): centralize mobile build number

---
+++ app.config.ts
@@ -0,0 +1,22 @@
+import type { ExpoConfig } from "expo/config";
+
+const baseConfig = require("./app.json").expo as ExpoConfig;
+
+// iOS buildNumber / Android versionCode를 한 곳에서 같이 관리합니다.
+const MOBILE_BUILD_NUMBER = 13;
+
+export default (): ExpoConfig => ({
+  ...baseConfig,
+  ios: {
+    ...baseConfig.ios,
+    buildNumber: String(MOBILE_BUILD_NUMBER),
+  },
+  android: {
+    ...baseConfig.android,
+    versionCode: MOBILE_BUILD_NUMBER,
+  },
+  extra: {
+    ...baseConfig.extra,
+    mobileBuildNumber: MOBILE_BUILD_NUMBER,
+  },
+});
